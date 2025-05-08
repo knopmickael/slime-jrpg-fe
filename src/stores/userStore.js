ae18ebe8fcaa1e5bed933c1c91f4a0ea20936f32
Mickael - desktop <knopmickael@gmail.com>
2025-05-08 15:30:13 -0300

    decodificando token no bootstrap da aplicação para recuperar os dados do usuário já autenticado

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,32 +1,62 @@
-import { defineStore } from 'pinia';
+import { defineStore } from "pinia";
 
-export const useUserStore = defineStore('user', {
-    state: () => ({
-        username: '',
-        loggedIn: false,
-        profilePicture: null,
-        token: null,
-    }),
-    actions: {
-        onBootLogIn() {
-            const token = localStorage.getItem('authToken');
-            if (token) {
-                this.token = token;
-                this.loggedIn = true;
-            }
-        },
-        defaultLogIn(username, profilePicture, token) {
-            this.username = username;
-            this.profilePicture = profilePicture;
-            this.loggedIn = true;
-            this.token = token;
-        },
-        logOut() {
-            this.username = '';
-            this.profilePicture = null;
-            this.loggedIn = false;
-            this.token = null;
-            localStorage.removeItem('authToken');
-        },
+export const useUserStore = defineStore("user", {
+  state: () => ({
+    token: null,
+    loggedIn: false,
+
+    usermail: "",
+    username: "",
+    profilePicture: null,
+  }),
+  actions: {
+    onBootLogIn() {
+      const token = localStorage.getItem("authToken");
+      if (token) {
+        try {
+          this.token = token;
+          this.loggedIn = true;
+
+          const decoded = parseJwt(token);
+          this.usermail = decoded.usermail;
+          this.username = decoded.username;
+          this.profilePicture = decoded.profilePicture;
+        } catch (error) {
+          console.error("Failed to decode token:", error);
+          this.logOut();
+        }
+      }
     },
+    defaultLogIn(usermail, username, profilePicture, token) {
+      this.token = token;
+      this.loggedIn = true;
+
+      this.usermail = usermail;
+      this.username = username;
+      this.profilePicture = profilePicture;
+    },
+    logOut() {
+      localStorage.removeItem("authToken");
+      this.token = null;
+      this.loggedIn = false;
+
+      this.usermail = "";
+      this.username = "";
+      this.profilePicture = null;
+    },
+  },
 });
+
+function parseJwt(token) {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map((c) => "%" + c.charCodeAt(0).toString(16).padStart(2, "0"))
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
